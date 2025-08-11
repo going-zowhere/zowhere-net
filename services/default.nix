bedee94a401d19124522d8af3f7ef7e15228ead1
Zoe <223009927+going-zowhere@users.noreply.github.com>
2025-08-11 02:25:45 -0600

    TEMP: disable broken configs for now

--- services/default.nix
+++ services/default.nix
@@ -1,9 +1,9 @@
 {config, ...}: {
   imports = [
-    ./agenix.nix
-    ./cloudflare.nix
-    ./copyparty.nix
-    ./soft-serve.nix
-    ./wastebin.nix
+    #./agenix.nix
+    #./cloudflare.nix
+    #./copyparty.nix
+    #./soft-serve.nix
+    #./wastebin.nix
   ];
 }
