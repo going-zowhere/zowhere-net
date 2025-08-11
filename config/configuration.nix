74fc42f04cd0bcf8a440a83c8159b9531c23d3f8
Zoe <223009927+going-zowhere@users.noreply.github.com>
2025-08-11 03:31:15 -0600

    Add back normal gc

--- config/configuration.nix
+++ config/configuration.nix
@@ -55,6 +55,13 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
+
+    # Enable garbage collection
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      persistent = true;
+    };
   };
 
   # Set the system time zone
