09b108d0fe1f1e8848489f6e1c315e0c730eaa73
Zoe <223009927+going-zowhere@users.noreply.github.com>
2025-08-11 03:02:09 -0600

    Remove gc conflict

--- config/configuration.nix
+++ config/configuration.nix
@@ -55,13 +55,6 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-
-    # Enable garbage collection
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      persistent = true;
-    };
   };
 
   # Set the system time zone
