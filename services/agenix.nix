2df5957d75e89610856b627e41474aba864deec9
Zoe <223009927+going-zowhere@users.noreply.github.com>
2025-08-12 01:23:33 -0600

    Try to fix secrets decryption

--- services/agenix.nix
+++ services/agenix.nix
@@ -1,24 +1,41 @@
 {config, ...}: {
   # Configure secrets
-  age.secrets = {
-    # Cloudflare tunnel
-    cloud = {
-      file = ../secrets/cloud.age;
-      path = "/home/zoe/.cloudflared/cert.pem";
-    };
-    tunnel = {
-      file = ../secrets/tunnel.age;
-      path = "/home/zoe/.cloudflared/985d435b-8a3b-46eb-a8a5-f92f15246e1e.json";
-    };
+  age = {
+    # Keys to use
+    identityPaths = [
+      "/home/zoe/.ssh/zowhere"
+      "/etc/ssh/ssh_host_ed25519_key"
+    ];
 
-    # Copyparty user passwords
-    cpp-zoe.file = ../secrets/cpp-zoe.age;
-    cpp-gue.file = ../secrets/cpp-gue.age;
+    # Secrets files
+    secrets = {
+      # Cloudflare tunnel
+      cloud = {
+        file = ../secrets/cloud.age;
+        path = "/home/zoe/.cloudflared/cert.pem";
+        owner = "zoe";
+        group = "users";
+        mode = "600";
+        symlink = false;
+      };
+      tunnel = {
+        file = ../secrets/tunnel.age;
+        path = "/home/zoe/.cloudflared/985d435b-8a3b-46eb-a8a5-f92f15246e1e.json";
+        owner = "zoe";
+        group = "users";
+        mode = "600";
+        symlink = false;
+      };
 
-    # FIXME Disabled for now
-    #waste = {
-    #  file = ../secrets/waste.age;
-    #  path = "/run/secrets/wastebin/wastebin.env";
-    #};
+      # Copyparty user passwords
+      cpp-zoe.file = ../secrets/cpp-zoe.age;
+      cpp-gue.file = ../secrets/cpp-gue.age;
+
+      # FIXME Disabled for now
+      #waste = {
+      #  file = ../secrets/waste.age;
+      #  path = "/run/secrets/wastebin/wastebin.env";
+      #};
+    };
   };
 }
